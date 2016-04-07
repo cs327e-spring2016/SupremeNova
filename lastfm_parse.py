e15727a2d8a5950e10515cf5f9092bc399947e3b
eforzano <ernestforzano@Ernests-MacBook-Pro.local>
2016-04-07 17:33:21 -0500

    Similar Artists with User Input
    When running the python script, the user can type an artist name, and
    the script returns a list of similar artists from Last.fm’s API.
    
    Side note: The API does not seem to be case sensitive.

--- lastfm_parse.py
+++ lastfm_parse.py
@@ -1,20 +1,31 @@
 from urllib.request import urlopen
 import json
 
+# user inputs artist name
+name = input("Input an artist name: ")
 
-# Get the dataset
-url = 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=Passion%20Pit&api_key=809d15fdb258f92ffd60f361dcf84feb&format=json'
+# replaces string spaces with %20 to fit API convention
+name.replace('','%20')
+
+# calls the last.fm API 
+url = 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=&api_key=809d15fdb258f92ffd60f361dcf84feb&format=json'
+url = url[:66] + (name) + url[66:]
 response = urlopen(url)
 
-# Convert bytes to string type and string type to dict
+# convert bytes to string type and string type to dict
 string = response.read().decode('utf-8')
 json_obj = json.loads(string)
+
+# creates artist list, last artist, and artist index
 artistList =[]
 artistLast = json_obj['similarartists']['artist'][-1]['name']
 artist_index = 0
+
+# iterates through the list and uses the last artist to prevent going out of index
 while json_obj['similarartists']['artist'][artist_index]['name'] is not artistLast:
 	artistList.append((json_obj['similarartists']['artist'][artist_index]['name']))
 	artist_index += 1
 
+# adds the last artist parsing through list
 artistList.append(artistLast)
 print(artistList)
