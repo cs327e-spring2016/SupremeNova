a21c5f512b8964b9a7c357d2ade1d7fbaea6ff8c
eforzano <ernestforzano@Ernests-MacBook-Pro.local>
2016-04-08 17:48:26 -0500

    Error Checking and Unlimited query
    This version allows for specific error checking and continues to allow
    the user to input data until the user solely presses Enter.

--- lastfm_parse.py
+++ lastfm_parse.py
@@ -2,30 +2,55 @@ from urllib.request import urlopen
 import json
 
 # user inputs artist name
-name = input("Input an artist name: ")
+name = str(input('Input an artist name (press ENTER to quit): '))
 
-# replaces string spaces with %20 to fit API convention
-name.replace('','%20')
 
-# calls the last.fm API 
-url = 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=&api_key=809d15fdb258f92ffd60f361dcf84feb&format=json'
-url = url[:66] + (name) + url[66:]
-response = urlopen(url)
 
-# convert bytes to string type and string type to dict
-string = response.read().decode('utf-8')
-json_obj = json.loads(string)
+# Continues loop until the user types quit
+while name is not '':
+	# replaces string spaces with %20 to fit API convention
+	name = name.replace(' ','%20')
 
-# creates artist list, last artist, and artist index
-artistList =[]
-artistLast = json_obj['similarartists']['artist'][-1]['name']
-artist_index = 0
+	# there is some type of error with when someone types a string '. H'
+	if ("." and "H") in name:
+		name = name.replace('.','')
 
-# iterates through the list and uses the last artist to prevent going out of index
-while json_obj['similarartists']['artist'][artist_index]['name'] is not artistLast:
-	artistList.append((json_obj['similarartists']['artist'][artist_index]['name']))
-	artist_index += 1
+	# calls the last.fm API 
+	url = 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=&api_key=809d15fdb258f92ffd60f361dcf84feb&format=json'
+	url = url[:66] + (name) + url[66:]
+	response = urlopen(url)
+	
+	# convert bytes to string type and string type to dict
+	string = response.read().decode('utf-8')
+	json_obj = json.loads(string)
 
-# adds the last artist parsing through list
-artistList.append(artistLast)
-print(artistList)
+	# error checking
+	#print((json_obj['similarartists']['artist']))
+
+	# to check if no artist is empty 
+	if ("error" not in string) and (len(json_obj['similarartists']['artist']) != 0):
+
+		# creates artist list, last artist, and artist index
+		artistList =[]
+		artistLast = json_obj['similarartists']['artist'][-1]['name']
+		artist_index = 0
+
+		# iterates through the list and uses the last artist to prevent going out of index
+		while json_obj['similarartists']['artist'][artist_index]['name'] is not artistLast:
+			artistList.append((json_obj['similarartists']['artist'][artist_index]['name']))
+			artist_index += 1
+
+		# adds the last artist parsing through list
+		artistList.append(artistLast)
+		print(artistList)
+		print()
+
+	# when no artist is found 
+	else:
+		print()
+		print("Similar Artists Not Found")
+		print()
+
+	# user inputs artist name
+	name = input('Input an artist name (press ENTER to quit): ')
+
